ca09880dd47196047266b57d81e35b0ebb3343e9
jcostadev23 <carlos_jesus_costa@hotmail.com>
2025-02-15 15:39:46 +0000

    array-methods-tests

---
+++ arrayMethods/arrayMethods.test.js
@@ -0,0 +1,39 @@
+const { groupByCategory, groupByCategory2 } = require("./arrayMethods");
+
+describe("groupedByCategory should organize elements by category", () => {
+  test("should return false if no elements", () => {
+    expect(groupByCategory([])).toBe(false);
+  });
+
+  test("should return a object with products organized by category", () => {
+    const products = [
+      { name: "Teclado", category: "Periféricos", price: 100 },
+      { name: "Cadeira", category: "Móveis", price: 1200 },
+    ];
+
+    const result = {
+      Periféricos: [{ name: "Teclado", category: "Periféricos", price: 100 }],
+      Móveis: [{ name: "Cadeira", category: "Móveis", price: 1200 }],
+    };
+
+    expect(groupByCategory(products)).toStrictEqual(result);
+  });
+
+  test("should return false if no elements", () => {
+    expect(groupByCategory2([])).toBe(false);
+  });
+
+  test("should return a object with products organized by category", () => {
+    const products = [
+      { name: "Costa", category: "$$$%%%", price: 100 },
+      { name: "Lilia", category: "&&$$", price: 1200 },
+    ];
+
+    const result = {
+      "$$$%%%": [{ name: "Costa", category: "$$$%%%", price: 100 }],
+      "&&$$": [{ name: "Lilia", category: "&&$$", price: 1200 }],
+    };
+
+    expect(groupByCategory2(products)).toStrictEqual(result);
+  });
+});
